e71179935608c7d75ed516052439ba2d90219b55
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-07-21 23:45:27 -0600

    creando la ruta /me protegida con el middleware auth para obtener la informacion del usuario actual

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
 const {
-  getUsers, getUserById, createUser, updateUser, changendAvatarUserMe, login,
+  getUsers, getUserById, getUserMe, createUser, updateUser, changendAvatarUserMe, login,
 } = require('../controllers/user');
+
+const { auth } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/singin', login);
 
 router.post('/singup', createUser);
+
+router.get('/me', auth, getUserMe);
 
 router.get('/', getUsers);
 
