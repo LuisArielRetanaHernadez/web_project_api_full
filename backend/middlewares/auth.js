f99cf2d2c884ca919d5fde50c0a1ebd178db4938
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-07-22 00:06:02 -0600

    importando el paquete jwt y sus credenciales. Agregamos la protecion si el Token no es valido y manejos retornamos el error corresponiente

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,11 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const { JWT_SECRET } = process.env;
+
+
 exports.auth = async (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization.split(' ')[1];
   if (!token) {
     const error = new Error('No token provided');
     error.statusCode = 401;
     throw error;
   }
   const decoded = jwt.verify(token, JWT_SECRET);
-  req.user = decoded;
-  next();
+  if (!decoded) {
+    error.statusCode = 403
+    const error = new Error('Not Authorization')
+    throw error
+  }
+
+  req.user = decoded
+  return next()
 };
