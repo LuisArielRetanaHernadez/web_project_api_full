cb3704ddbf80fa180e0edca1186db2297c5d4bce
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-07-24 15:57:10 -0600

    pasando las rutas de dar like o dislike a un card arriba de los middleware auth y protectCard

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -11,12 +11,14 @@ const router = express.Router();
 
 router.get('/', getCards);
 
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
+
 router.use(auth)
 router.post('/', createCard);
 
 router.use(protectCard)
 router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+
 
 module.exports = { cardRouter: router };
