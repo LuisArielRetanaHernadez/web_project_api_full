91f9668f01635cfb0ad80ccdf16e4a348ff2cb99
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-07-25 17:37:38 -0600

    Agregando envolviendo los controladores del usuario con la funcion asyncHandler

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,22 +6,24 @@ const {
 
 const { auth } = require('../middlewares/auth');
 
+const asyncHandler = require('../utils/asyncHandler');
+
 const router = express.Router();
 
-router.post('/singin', login);
+router.post('/singin', asyncHandler(login));
 
-router.post('/singup', createUser);
+router.post('/singup', asyncHandler(createUser));
 
 router.use(auth)
 
-router.get('/', getUsers);
+router.get('/', asyncHandler(getUsers));
 
-router.get('/me', getUserMe);
+router.get('/me', asyncHandler(getUserMe));
 
-router.get('/:id', getUserById);
+router.get('/:id', asyncHandler(getUserById));
 
-router.put('/me', updateUser);
+router.put('/me', asyncHandler(updateUser));
 
-router.patch('/me/avatar', (changendAvatarUserMe));
+router.patch('/me/avatar', asyncHandler(changendAvatarUserMe));
 
 module.exports = { userRouter: router };
