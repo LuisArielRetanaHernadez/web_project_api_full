6f036335bb1a51469a77dbdd3842bd1b64effc69
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-07-25 18:47:13 -0600

    agregando las instacias de erros y quitamos los bloques try...catch

--- backend/controllers/card.js
+++ backend/controllers/card.js
@@ -1,86 +1,68 @@
 /* eslint-disable linebreak-style */
 const card = require('../models/card');
+const { NotFoundError, ForbiddenError, InternalServerError } = require('../utils/managerErrors');
 
 exports.getCards = async (req, res) => {
-  await card.find({})
-    .orFail(() => {
-      const error = new Error('No se encontraron cartas');
-      error.statusCode = 404;
-      throw error;
-    })
-    .then((cardsFound) => res.status(200).json({ data: cardsFound }))
-    .catch((err) => res.status(err.statusCode).json({ message: err.message }));
+  const cardsFound = await card.find({});
+
+  if (!cardsFound || cardsFound.length === 0) {
+    throw new NotFoundError('No se encontraron cartas');
+  }
+
+  res.status(200).json({ data: cardsFound });
 };
 
 exports.deleteCard = async (req, res) => {
-  try {
-    const cardDelete = await card.findById(req.params.cardId);
+  const cardDelete = await card.findById(req.params.cardId);
 
-    if (!cardDelete) {
-      const error = new Error('No se encontró la carta');
-      error.statusCode = 404;
-      throw error;
-    }
-    if (cardDelete.owner.toString() !== req.user._id) {
-      const error = new Error('No tienes permiso para eliminar esta carta');
-      error.statusCode = 403;
-      throw error;
-    }
-    await card.deleteOne({ _id: req.params.cardId });
-    res.status(200).json({ message: 'Carta eliminada' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  if (!cardDelete) {
+    throw new NotFoundError('No se encontró la carta');
   }
+
+  if (cardDelete.owner.toString() !== req.user.id) {
+    throw new ForbiddenError('No tienes permiso para eliminar esta carta');
+  }
+
+  await card.deleteOne({ _id: req.params.cardId });
+  res.status(200).json({ message: 'Carta eliminada' });
 };
 
 exports.createCard = async (req, res) => {
-  try {
-    const idUser = req.user.id
+  const idUser = req.user.id;
 
-    const newCard = await card.create({...req.body, owner: idUser });
-    if (!newCard) {
-      const error = new Error('No se pudo crear la carta');
-      error.statusCode = 500;
-      throw error;
-    }
-    res.status(201).json(newCard);
-  } catch (error) {
-    res.status(error.statusCode || 400).json({ message: error.message });
+  const newCard = await card.create({ ...req.body, owner: idUser });
+
+  if (!newCard) {
+    throw new InternalServerError('No se pudo crear la carta');
   }
+
+  res.status(201).json(newCard);
 };
 
 exports.likeCard = async (req, res) => {
-  try {
-    const updatedCard = await card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-    );
-    if (!updatedCard) {
-      const error = new Error('No se pudo actualizar la carta');
-      error.statusCode = 500;
-      throw error;
-    }
-    res.status(200).json(updatedCard);
-  } catch (error) {
-    res.status(error.statusCode).json({ message: error.message });
+  const updatedCard = await card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user.id } },
+    { new: true },
+  );
+
+  if (!updatedCard) {
+    throw new InternalServerError('No se pudo actualizar la carta');
   }
+
+  res.status(200).json(updatedCard);
 };
 
 exports.dislikeCard = async (req, res) => {
-  try {
-    const updatedCard = await card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } },
-      { new: true },
-    );
-    if (!updatedCard) {
-      const error = new Error('No se pudo actualizar la carta');
-      error.statusCode = 500;
-      throw error;
-    }
-    res.status(200).json(updatedCard);
-  } catch (error) {
-    res.status(error.statusCode).json({ message: error.message });
+  const updatedCard = await card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user.id } },
+    { new: true },
+  );
+
+  if (!updatedCard) {
+    throw new InternalServerError('No se pudo actualizar la carta');
   }
+
+  res.status(200).json(updatedCard);
 };
