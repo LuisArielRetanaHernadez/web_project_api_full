d1bb10e9a5b71f97e0e29c915aaddb87a45891ef
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-07-27 17:52:04 -0600

    arrojando los errores con las instancias de errores creadas

--- backend/middlewares/protectCard.js
+++ backend/middlewares/protectCard.js
@@ -1,24 +1,22 @@
-const Card = require('../models/card')
+const Card = require('../models/card');
+const { BadRequestError, ForbiddenError } = require('../utils/managerErrors');
 
 exports.protectCard = async (req, res, next) => {
-    if (req.params.cardId == null) {
-        const error = new Error('ID card no valido')
-        error.statusCode = 401
-        throw error
-    }
+  const { cardId } = req.params;
 
-    const getCard = await Card.find({
-        _id: req.params.id,
-        owner: req.user._id
-    })
+  if (!cardId) {
+    throw new BadRequestError('ID de carta no válido');
+  }
 
-    if (!getCard) {
-        const error = new Error('Action not Autorization')
-        error.statusCode = 401
-        throw error
-    }
+  const getCard = await Card.findOne({
+    _id: cardId,
+    owner: req.user.id,
+  });
 
-    req.card = getCard
+  if (!getCard) {
+    throw new ForbiddenError('No tienes permiso para acceder a esta carta');
+  }
 
-    return next()
-}
+  req.card = getCard;
+  return next();
+};
