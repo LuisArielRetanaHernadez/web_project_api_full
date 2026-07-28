951c63ee1c7fe2d8b957488957f3240784851e0c
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-07-27 19:51:25 -0600

    agregando los validares de login, register, update y updateAvatar en los routers del usuario

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,17 +13,24 @@ const {
 const { auth } = require('../middlewares/auth');
 const asyncHandler = require('../utils/asyncHandler');
 
+const {
+  registerUserSchema,
+  loginUserSchema,
+  updateUserSchema,
+  updateAvatarSchema,
+} = require('../middlewares/celebrate/user');
+
 const router = express.Router();
 
-router.post('/signin', asyncHandler(login));
-router.post('/signup', asyncHandler(createUser));
+router.post('/signin', loginUserSchema, asyncHandler(login));
+router.post('/signup', registerUserSchema, asyncHandler(createUser));
 
 router.use(asyncHandler(auth));
 
 router.get('/', asyncHandler(getUsers));
 router.get('/me', asyncHandler(getUserMe));
 router.get('/:id', asyncHandler(getUserById));
-router.put('/me', asyncHandler(updateUser));
-router.patch('/me/avatar', asyncHandler(changendAvatarUserMe));
+router.put('/me', updateUserSchema, asyncHandler(updateUser));
+router.patch('/me/avatar', updateAvatarSchema, asyncHandler(changendAvatarUserMe));
 
 module.exports = { userRouter: router };
