50028714766694fd6d5aa9f9a6e06365edf6f598
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-08-01 18:25:06 -0600

    creando el archivo logger de registro de solicitud del servidor

---
+++ backend/middlewares/logger.js
@@ -0,0 +1,26 @@
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
+const requestLogger = expressWinston.logger({
+    transports: [
+        new winston.transports.File({ filename: 'logs/request.log' }),
+    ],
+    format: winston.format.json(),
+});
+
+module.exports = { requestLogger };
+
+// const logger = expressWinston.logger({
+//   transports: [
+//     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+//     new winston.transports.File({ filename: 'logs/combined.log' }),
+//     ],
+//     format: winston.format.combine(
+//         winston.format.timestamp(),
+//         winston.format.json(),
+//     ),
+//     meta: true,
+//     msg: 'HTTP {{req.method}} {{req.url}}',
+//     expressFormat: true,
+//     colorize: false,
+// });
