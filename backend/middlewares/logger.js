e3734468608d76679de92c0130dc3ff661ebe3fe
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-08-01 19:44:08 -0600

    cambiando el formato de los loggers de requeste del servidor y de los errores

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -5,14 +5,29 @@ const requestLogger = expressWinston.logger({
     transports: [
         new winston.transports.File({ filename: 'logs/request.log' }),
     ],
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
+    meta: true,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    expressFormat: true,
+    colorize: false,
+
 });
 
 const errorLogger = expressWinston.errorLogger({
     transports: [
         new winston.transports.File({ filename: 'logs/error.log' }),
     ],
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
+    meta: true,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    expressFormat: true,
+    colorize: false,
 });
 
 module.exports = { requestLogger, errorLogger };
