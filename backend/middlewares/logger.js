669935c898b76aa5960a7cdf765d87d0f9a0a1fb
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-08-19 20:17:07 -0600

    agregando axiomh1/winston en el transporte de log en el modo produccion

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,10 +1,30 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
+const { WinstonTransport } = require('@axiomhq/winston');
+
+require('dotenv').config();
+
+// configurar winston con axiom para el modo producción y utilizar winston sin axiom para el modo desarrollador
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const transports = (filename) => {
+    return NODE_ENV === 'production'
+        ? [
+            new WinstonTransport({
+                token: process.env.AXIOM_TOKEN,
+                dataset: process.env.AXIOM_DATASET,
+                edge: 'eu-central-1.aws.edge.axiom.co',
+            }),
+        ]
+        : [
+            new winston.transports.File({ filename }),
+        ];
+}
+
 
 const requestLogger = expressWinston.logger({
-    transports: [
-        new winston.transports.File({ filename: 'logs/request.log' }),
-    ],
+    level: 'info',
+    transports: transports('logs/request.log'),
     format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.json(),
@@ -16,14 +36,22 @@ const requestLogger = expressWinston.logger({
 
 });
 
-const errorLogger = expressWinston.errorLogger({
-    transports: [
-        new winston.transports.File({ filename: 'logs/error.log' }),
-    ],
-    format: winston.format.combine(
+const formatError = NODE_ENV === 'production'
+    ? winston.format.combine(
         winston.format.timestamp(),
         winston.format.json(),
-    ),
+    )
+    : winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
+        winston.format.json(),
+    );
+
+const errorLogger = expressWinston.errorLogger({
+    transports: transports('logs/error.log'),
+    exceptionHandlers: transports('logs/exceptions.log'),
+    rejectionHandlers: transports('logs/rejections.log'),
+    format: formatError,
     meta: true,
     msg: 'HTTP {{req.method}} {{req.url}}',
     expressFormat: true,
